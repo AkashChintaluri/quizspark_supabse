b867fc4880b2f1bcf34b7f91fc35b331f5fe3e63
Chintaluri Akash <akashchintaluriofficial@gmail.com>
2025-04-16 20:50:14 +0530

    Deploying Backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,9 @@ terraform {
       version = "~> 4.0"
     }
   }
+  backend "local" {
+    path = "terraform.tfstate"
+  }
 }
 
 provider "aws" {
@@ -13,6 +16,13 @@ provider "aws" {
 
 data "aws_security_group" "existing_sg" {
   name = "quizspark-sg"
+}
+
+data "aws_instance" "existing_instance" {
+  filter {
+    name   = "tag:Name"
+    values = ["quizspark-backend"]
+  }
 }
 
 resource "random_id" "sg_suffix" {
@@ -46,6 +56,8 @@ resource "aws_security_group" "quizspark_sg" {
 }
 
 resource "aws_instance" "quizspark_backend" {
+  count = data.aws_instance.existing_instance.id == null ? 1 : 0
+
   ami                    = "ami-0f5ee92e2d63afc18" # Ubuntu 20.04 LTS in ap-south-1
   instance_type          = "t2.micro"
   vpc_security_group_ids = [data.aws_security_group.existing_sg.id]
@@ -64,9 +76,9 @@ resource "aws_instance" "quizspark_backend" {
 }
 
 output "instance_public_ip" {
-  value = aws_instance.quizspark_backend.public_ip
+  value = data.aws_instance.existing_instance.id != null ? data.aws_instance.existing_instance.public_ip : aws_instance.quizspark_backend[0].public_ip
 }
 
 output "instance_public_dns" {
-  value = aws_instance.quizspark_backend.public_dns
+  value = data.aws_instance.existing_instance.id != null ? data.aws_instance.existing_instance.public_dns : aws_instance.quizspark_backend[0].public_dns
 } 
